2a71cac5dd23f6686161b3226152b27d8eedbff0
kaola <kaola2024@gmail.com>
2024-04-27 16:22:24 +0800

    使用panel组件

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,5 @@ import httpInstance from "@/utiles/http.js";
 export const reqGetCategory = ()=> httpInstance.get('/home/category/head')
 //首页轮播图 /home/banner
 export const reqGetBannerInfo = ()=> httpInstance.get('/home/banner')
+//获取-新鲜好物  /home/new
+export const reqGetNewGoods = ()=> httpInstance.get('/home/new')
