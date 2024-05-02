d2b5a83ad5a4deb957274d69eeb9aa08847e1d49
kaola <kaola2024@gmail.com>
2024-05-02 17:04:00 +0800

    登录--请求拦截器携带token

--- src/utiles/http.js
+++ src/utiles/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useUserStore} from "@/stores/user.js";
 
 const httpInstance = axios.create({
   baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -12,6 +13,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  //1. 从pinia获取token
+  const userStore = useUserStore()
+  const token = userStore.userInfo.token
+  //2. 按后端要求拼接token数据
+  if (token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
