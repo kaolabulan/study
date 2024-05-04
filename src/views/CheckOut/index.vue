a9116816a366b76e1497ae26a57854a97565fa10
kaola <kaola2024@gmail.com>
2024-05-04 15:16:13 +0800

    结算--弹窗地址切换

--- src/views/CheckOut/index.vue
+++ src/views/CheckOut/index.vue
@@ -10,6 +10,20 @@ const getCheckoutInfo =async ()=>{
   curAddress.value = checkInfo.value.userAddresses.find(item=>item.isDefault === 0)
 }
 onMounted(()=>getCheckoutInfo())
+
+//切换地址
+const showDialog = ref(false)  //弹窗控制
+const activeAddress = ref({})
+const switchAddress = (item)=>{
+  activeAddress.value = item
+}
+//绑定点击确定事件
+const confirm = ()=>{
+  curAddress.value = activeAddress.value
+  //关闭弹窗
+  showDialog.value=false
+  activeAddress.value={}
+}
 </script>
 
 <template>
@@ -29,7 +43,7 @@ onMounted(()=>getCheckoutInfo())
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true">切换地址</el-button>
+              <el-button size="large" @click="showDialog = true">切换地址</el-button>
               <el-button size="large" @click="addFlag = true">添加地址</el-button>
             </div>
           </div>
@@ -110,6 +124,23 @@ onMounted(()=>getCheckoutInfo())
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" :class="{active:activeAddress.id===item.id}" @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+    <span class="dialog-footer">
+      <el-button>取消</el-button>
+      <el-button type="primary" @click="confirm">确定</el-button>
+    </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
